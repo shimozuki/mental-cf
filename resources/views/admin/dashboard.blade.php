1c896d2450845f51e0547f9ab4b1a7f3c6aacac1
robbishimozuki <robbishimozuki@gmail.com>
2025-07-23 23:43:40 +0800

    ok done

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -93,37 +93,6 @@
           </div><!-- End Revenue Card -->
 
           <!-- Customers Card -->
-          <div class="col-xxl-4 col-xl-12">
-
-            <div class="card info-card customers-card">
-
-              <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-                </ul>
-              </div>
-
-              <div class="card-body">
-                <h5 class="card-title">Jumlah <span>| Admin</span></h5>
-
-                <div class="d-flex align-items-center">
-                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-people"></i>
-                  </div>
-                  <div class="ps-3">
-                    <h6>{{$user->count()}}</h6>
-                    <span class="text-danger small pt-1 fw-bold">{{$user->count()}}</span> <span class="text-muted small pt-2 ps-1">admin</span>
-
-                  </div>
-                </div>
-
-              </div>
-            </div>
-
-          </div><!-- End Customers Card -->
 
 
 
